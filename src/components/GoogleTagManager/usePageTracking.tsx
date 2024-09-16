f5f38a935d4fa174f5dd74b3378edacb179b17e9
abhinay <abhinay@scribble-werbeagentur.de>
2024-09-16 21:59:18 +0200

    cookie consent form Google tag manager component added

---
+++ src/components/GoogleTagManager/usePageTracking.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export const usePageTracking = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    (window as any).dataLayer = (window as any).dataLayer || [];
+    (window as any).dataLayer.push({
+      event: 'pageview',
+      page: location.pathname,
+    });
+  }, [location]);
+};

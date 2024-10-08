c97eb4425c93d4dd887eb60f937fda6cd225ff9b
abhinay khalatkar <abhinaykhalatkar@gmail.com>
2024-10-08 09:11:38 +0200

    scroll to top buttion z index chnage for ,contact route added with some more styling changes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,9 +8,11 @@ const Footer: React.FC = () => {
         <div className={styles.Footer}>
             <p>&copy; {new Date().getFullYear()} {companyName}. All rights reserved.</p>
             <ul className={styles.footerLinks}>
-                <li><a href="/privacy">Privacy Policy</a></li>
-                <li><a href="/terms">Terms of Service</a></li>
-                <li><a href="/contact">Contact Us</a></li>
+
+                <li><Link to="/datenschutz">Privacy Policy</Link></li>
+                <li><Link to="/impressum">Terms of Service</Link></li>
+                <li><Link to="/contact">Contact Us</Link></li>
+
             </ul>
 
         </div>

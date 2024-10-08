fb36b64a7bab2893ff05edca70245cc7e67bc58a
abhinay khalatkar <abhinaykhalatkar@gmail.com>
2024-10-08 10:57:30 +0200

    gitworkflow test

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ function App() {
   return (
     <div className="App">
       {/* <EditorMain/> */}
+      {/* git workflow test */}
       <CookieConsentForm />
       <ScrollToTopFunction />
       <AccessibilityMenu />

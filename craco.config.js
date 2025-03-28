81db7e9704a10af0ae01c7643e4cd884e5c681b5
Abhinay Khalatkar <abhinaykhalatkar@gmail.com>
2025-03-28 10:09:09 +0100

    dart upgrade scss and scss loader

--- craco.config.js
+++ craco.config.js
@@ -9,6 +9,22 @@ module.exports = {
       const oneOfRule = webpackConfig.module.rules.find((rule) => rule.oneOf);
       if (!oneOfRule) return webpackConfig;
 
+      // Override CRA's outdated sass-loader with the latest version
+      oneOfRule.oneOf.forEach((rule) => {
+        if (rule.use) {
+          rule.use.forEach((loader) => {
+            if (loader.loader && loader.loader.includes('sass-loader')) {
+              loader.loader = require.resolve('sass-loader');
+              loader.options = {
+                sourceMap: true,
+                implementation: require('sass'),
+              };
+            }
+          });
+        }
+      });
+
+      // Existing responsive-loader config
       oneOfRule.oneOf.unshift({
         test: /\.(jpe?g|png|gif)$/i,
         use: {

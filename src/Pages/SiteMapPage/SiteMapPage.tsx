7f3566cc2391eeffb73d0243533c6b976ad77275
Abhinay Khalatkar <abhinaykhalatkar@gmail.com>
2025-06-17 20:47:08 +0200

    sitemap page updated

--- src/Pages/SiteMapPage/SiteMapPage.tsx
+++ src/Pages/SiteMapPage/SiteMapPage.tsx
@@ -3,40 +3,16 @@
 import styles from "./SiteMapPage.module.scss";
 import { Link } from "react-router-dom";
 import routesData, { RouteData } from "../routerData";
-
-const generateSiteMap = (routes: RouteData[], parentPath = ''): React.ReactNode => {
-  return routes.map((route) => {
-
-    if (route.addToSiteMap === false) return null;
-
-    const fullPath = `${parentPath}${route.path}`; // Concatenate the parent path with the route's path
+import ComponentContainer from "@UI/ComponentContainer/ComponentContainer";
 
 
-    if (route.subRoutes && route.subRoutes.length > 0) {
-      return (
-        <li key={fullPath} className={styles.item}>
-          {route.navName ? <Link to="#">{route.navName}</Link> : null}
-          <ul>
-            {generateSiteMap(route.subRoutes, fullPath)}
-          </ul>
-        </li>
-      );
-    }
-
-    return (
-      <li key={fullPath} className={styles.item}>
-        {route.navName && <Link to={fullPath}>{route.navName}</Link>}
-      </li>
-    );
-  });
-};
 
 const generateSubRoutes = (subRoutes: RouteData[], parentPath: string): React.ReactNode => {
   return subRoutes.map((subRoute) => {
     if (subRoute.addToSiteMap === false) return null;
 
     const fullPath = `${parentPath}${subRoute.path}`;
-    
+
     return (
       <li key={fullPath} className={styles.subItem}>
         <Link to={fullPath}>{subRoute.navName}</Link>
@@ -47,36 +23,63 @@ const generateSubRoutes = (subRoutes: RouteData[], parentPath: string): React.Re
 
 export default function SiteMapPage() {
   return (
-    <>
-      <section className={"d-flex flex-column " + styles.siteMap}>
-        <h1>Sitemap</h1>
-        <nav className={styles.sitemap}>
-          <ul className={routesData.length>10? styles.setColumn:""}>
-            {routesData.map((route) => {
-              if (route.addToSiteMap === false) return null;
+    <ComponentContainer heading="" className={`flex flex-col ${styles.siteMap} max-w-fit justify-self-center`} containerClassName="">
+      <h1 className="my-8">Sitemap</h1>
+      <nav className={styles.sitemap}>
+        <ul className={routesData.length > 10 ? styles.setColumn : ""}>
+          {routesData.map((route) => {
+            if (route.addToSiteMap === false) return null;
 
-              const fullPath = route.path;
+            const fullPath = route.path;
 
-              if (route.subRoutes && route.subRoutes.length > 0) {
-                return (
-                  <li key={fullPath} className={styles.item}>
-                    {route.navName ? <Link to="#">{route.navName}</Link> : null}
-                    <ul>
-                      {generateSubRoutes(route.subRoutes, fullPath)}
-                    </ul>
-                  </li>
-                );
-              }
-
+            if (route.subRoutes && route.subRoutes.length > 0) {
               return (
                 <li key={fullPath} className={styles.item}>
-                  <Link to={fullPath}>{route.navName}</Link>
+                  {route.navName ? <Link to="#">{route.navName}</Link> : null}
+                  <ul>
+                    {generateSubRoutes(route.subRoutes, fullPath)}
+                  </ul>
                 </li>
               );
-            })}
-          </ul>
-        </nav>
-      </section>
-    </>
+            }
+
+            return (
+              <li key={fullPath} className={styles.item}>
+                <Link to={fullPath}>{route.navName}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </ComponentContainer>
   );
 }
+
+
+
+// const generateSiteMap = (routes: RouteData[], parentPath = ''): React.ReactNode => {
+//   return routes.map((route) => {
+
+//     if (route.addToSiteMap === false) return null;
+
+//     const fullPath = `${parentPath}${route.path}`; // Concatenate the parent path with the route's path
+
+
+//     if (route.subRoutes && route.subRoutes.length > 0) {
+//       return (
+//         <li key={fullPath} className={styles.item}>
+//           {route.navName ? <Link to="#">{route.navName}</Link> : null}
+//           <ul>
+//             {generateSiteMap(route.subRoutes, fullPath)}
+//           </ul>
+//         </li>
+//       );
+//     }
+
+//     return (
+//       <li key={fullPath} className={styles.item}>
+//         {route.navName && <Link to={fullPath}>{route.navName}</Link>}
+//       </li>
+//     );
+//   });
+// };
